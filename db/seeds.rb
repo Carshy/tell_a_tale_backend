bb094df6975211b44ea28f0e176485a8509f2188
Collins Musoko <mussocollins@gmail.com>
2023-12-20 13:52:27 +0300

    Add schemas to User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.create(email: 'carshycollins@gmail.com', password: 'carshy1234', first_name: 'Carshy', last_name: 'Collins', bio: 'A seasoned full stack developer', photo: 'https://i.pngimg.me/thumb/f/720/a8bd1f9386.jpg')
+user1 = User.create(email: 'nesslarry@gmail.com', password: 'carshy1234', first_name: 'Ness', last_name: 'Larry', bio: 'A seasoned full stack developer creating scalable and dynamic web applications', photo: 'https://i.pngimg.me/thumb/f/720/a8bd1f9386.jpg')
+user2 = User.create(email: 'messmarry@gmail.com', password: 'carshy1234', first_name: 'Mess', last_name: 'Marry', bio: 'A seasoned full stack developer creating scalable and dynamic web applications', photo: 'https://i.pngimg.me/thumb/f/720/a8bd1f9386.jpg')
