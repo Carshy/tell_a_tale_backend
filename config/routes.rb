463929343f6a4997097a49ef826ded061e29b111
Collins Musoko <mussocollins@gmail.com>
2023-12-22 05:58:59 +0300

    Set authentication security

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,22 @@ Rails.application.routes.draw do
     sessions: 'api/v1/users/sessions',
     registrations: 'api/v1/users/registrations'
   }
-
-  namespace :auth, path: 'api/v1/users' do
-    devise_scope :user do
-      post 'sign_in', to: 'sessions#create'
-      delete 'sign_out', to: 'sessions#destroy'
+  
+  namespace :api do
+    namespace :v1 do
+      devise_scope :user do
+        post 'sign_up', to: 'users/registrations#create'
+        post 'sign_in', to: 'users/sessions#create'
+        delete 'sign_out', to: 'users/sessions#destroy'
+      end
     end
   end
+
+  # namespace :auth, path: 'api/v1/users' do
+  #   devise_scope :user do
+  #     post 'sign_in', to: 'sessions#create'
+  #     delete 'sign_out', to: 'sessions#destroy'
+  #   end
+  # end
   
 end
