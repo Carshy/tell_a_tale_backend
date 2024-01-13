e4b0cedf1fe9dfaf3b459fa3db8cb66a55272787
Collins Musoko <mussocollins@gmail.com>
2024-01-13 06:22:21 +0300

    Run test to test password encryption

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,8 +12,18 @@ RSpec.describe User, type: :model do
   end
 
   it "requires a unique email address" do
-    existing_user = create(:user, email: "semirem@gmail.com")
-    new_user = build(:user, email: "semirem@gmail.com")
+    existing_user = User.create(email: "semirem@gmail.com", password: "carshy1234")
+    new_user = User.new(email: "semirem@gmail.com", password: "carshy123")
     expect(new_user).to_not be_valid
+  end
+
+  it "encrypts password before saving" do
+    user = User.create(email: "encrypt@gmail.com", password: "carshy1234")
+  
+    # Reload the user from the database to ensure you're working with the persisted version
+    user_from_db = User.find(user.id)
+  
+    # Check if the stored password matches the plain text password
+    expect(user_from_db.password).to_not eq("carshy1234")
   end
 end
