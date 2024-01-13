4be605fa6425dd05ce3c5ecab283dcdeb016bbbe
Collins Musoko <mussocollins@gmail.com>
2024-01-13 05:15:55 +0300

    Add email uniqueness validation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+  validates :email, uniqueness: true
   has_many :posts
   has_many :comments
 end
