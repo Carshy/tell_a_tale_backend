c05779a32e0b1bf271b76d1a60dd1e52c5834339
Collins Musoko <mussocollins@gmail.com>
2024-01-13 04:57:21 +0300

    Add test suites for unique email in models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe User, type: :model do
     user = User.new(email: nil)
     expect(user).to_not be_valid
   end
+
+  it "is not valid without a password" do
+    user = User.new(password: nil)
+    expect(user).to_not be_valid
+  end
+
+  it "requires a unique email address" do
+    existing_user = create(:user, email: "semirem@gmail.com")
+    new_user = build(:user, email: "semirem@gmail.com")
+    expect(new_user).to_not be_valid
+  end
 end
