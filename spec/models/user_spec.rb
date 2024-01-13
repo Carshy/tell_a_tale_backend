44fd8c6fb22099d368b860fb3c3169e7abb6699b
Collins Musoko <mussocollins@gmail.com>
2024-01-13 13:58:11 +0300

    Correct linters

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is not valid without an email" do
+  it 'is not valid without an email' do
     user = User.new(email: nil)
     expect(user).to_not be_valid
   end
 
-  it "is not valid without a password" do
+  it 'is not valid without a password' do
     user = User.new(password: nil)
     expect(user).to_not be_valid
   end
 
-  it "requires a unique email address" do
-    existing_user = User.create(email: "semirem@gmail.com", password: "carshy1234")
-    new_user = User.new(email: "semirem@gmail.com", password: "carshy123")
+  it 'requires a unique email address' do
+    User.create(email: 'semirem@gmail.com', password: 'carshy1234')
+    new_user = User.new(email: 'semirem@gmail.com', password: 'carshy123')
     expect(new_user).to_not be_valid
   end
 
-  it "encrypts password before saving" do
-    user = User.create(email: "encrypt@gmail.com", password: "carshy1234")
-  
+  it 'encrypts password before saving' do
+    user = User.create(email: 'encrypt@gmail.com', password: 'carshy1234')
+
     # Reload the user from the database to ensure you're working with the persisted version
     user_from_db = User.find(user.id)
-  
+
     # Check if the stored password matches the plain text password
-    expect(user_from_db.password).to_not eq("carshy1234")
+    expect(user_from_db.password).to_not eq('carshy1234')
   end
 end
